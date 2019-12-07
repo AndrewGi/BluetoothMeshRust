482828097c951fb75ac37158f60397c171e75d84
AndrewGi <andrew@gilbrough.com>
2019-12-06 20:15:10 -0800

    first steps to a bt mesh library

---
+++ src/address.rs
@@ -0,0 +1,4 @@
+pub struct UnicastAddress(u16);
+pub struct GroupAddress(u16);
+pub struct VirtualAddressHash(u16);
+pub struct VirtualAddress(VirtualAddressHash)
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![no_std]
+mod bytes;
 #[cfg(test)]
 mod tests {
     #[test]
