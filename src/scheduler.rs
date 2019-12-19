c5c106a899f82cc39a78f9b2a047fc328d6827a5
Andrew <andrew@gilbrough.com>
2019-12-18 22:51:22 -0800

    started work on a scheduler.rs for packet and publishing scheduling. Current no sync/locking on it but it could be wrapped in a Mutex or something. Also added some BLE Gatt characteristics.rs for Mesh properties.

---
+++ src/scheduler.rs
@@ -0,0 +1,183 @@
+use crate::time::Timestamp;
+use alloc::collections::BinaryHeap;
+use core::cmp::Ordering;
+use core::time::Duration;
+
+#[derive(Debug)]
+pub struct TimeQueueEntry<T> {
+    timestamp: Timestamp,
+    item: T,
+}
+impl<T> TimeQueueEntry<T> {
+    pub fn new(timestamp: Timestamp, item: T) -> TimeQueueEntry<T> {
+        TimeQueueEntry { timestamp, item }
+    }
+    pub fn timestamp(&self) -> Timestamp {
+        self.timestamp
+    }
+}
+impl<T: Clone> Clone for TimeQueueEntry<T> {
+    fn clone(&self) -> Self {
+        TimeQueueEntry {
+            timestamp: self.timestamp.clone(),
+            item: self.item.clone(),
+        }
+    }
+}
+impl<T> AsRef<T> for TimeQueueEntry<T> {
+    fn as_ref(&self) -> &T {
+        &self.item
+    }
+}
+impl<T> AsMut<T> for TimeQueueEntry<T> {
+    fn as_mut(&mut self) -> &mut T {
+        &mut self.item
+    }
+}
+impl<T> PartialOrd for TimeQueueEntry<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.timestamp.partial_cmp(&other.timestamp)
+    }
+}
+impl<T> PartialEq for TimeQueueEntry<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.timestamp.eq(&other.timestamp)
+    }
+}
+impl<T> Eq for TimeQueueEntry<T> {}
+impl<T> Ord for TimeQueueEntry<T> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.timestamp.cmp(&other.timestamp)
+    }
+}
+#[derive(Debug, Default)]
+pub struct TimeQueue<T> {
+    priority_queue: BinaryHeap<TimeQueueEntry<T>>,
+}
+impl<T: Clone> Clone for TimeQueue<T> {
+    fn clone(&self) -> Self {
+        TimeQueue {
+            priority_queue: self.priority_queue.clone(),
+        }
+    }
+}
+impl<T> TimeQueue<T> {
+    pub fn new() -> TimeQueue<T> {
+        TimeQueue {
+            priority_queue: Default::default(),
+        }
+    }
+    pub fn with_capacity(capacity: usize) -> TimeQueue<T> {
+        TimeQueue {
+            priority_queue: BinaryHeap::with_capacity(capacity),
+        }
+    }
+    pub fn len(&self) -> usize {
+        self.priority_queue.len()
+    }
+    pub fn peek(&self) -> Option<&TimeQueueEntry<T>> {
+        self.priority_queue.peek()
+    }
+    pub fn peek_item(&self) -> Option<&T> {
+        Some(&self.peek()?.as_ref())
+    }
+    pub fn peek_timestamp(&self) -> Option<Timestamp> {
+        Some(self.peek()?.timestamp())
+    }
+    /*
+    pub fn peek_item_mut(&mut self) -> Option<&mut T> {
+        Some(&mut self.priority_queue.peek_mut()?.item)
+    }
+    */
+    pub fn push(&mut self, timestamp: Timestamp, item: T) {
+        self.priority_queue
+            .push(TimeQueueEntry::new(timestamp, item))
+    }
+    pub fn pop(&mut self) -> Option<TimeQueueEntry<T>> {
+        self.priority_queue.pop()
+    }
+    pub fn pop_item(&mut self) -> Option<T> {
+        Some(self.pop()?.item)
+    }
+    pub fn time_until_next(&self) -> Option<Duration> {
+        self.peek_timestamp()?.duration_until(&Timestamp::now())
+    }
+    pub fn is_empty(&self) -> bool {
+        self.priority_queue.is_empty()
+    }
+    pub fn next_is_ready(&self) -> bool {
+        !self.is_empty() && self.time_until_next().is_none()
+    }
+    pub fn map_ready_item(&mut self, mut func: impl FnMut(T)) {
+        while let Some(i) = self.pop_item() {
+            func(i)
+        }
+    }
+    pub fn clear(&mut self) {
+        self.priority_queue.clear()
+    }
+    pub fn reserve(&mut self, additional: usize) {
+        self.priority_queue.reserve(additional)
+    }
+    pub fn shrink_to_fit(&mut self) {
+        self.priority_queue.shrink_to_fit()
+    }
+    pub fn into_heap(self) -> BinaryHeap<TimeQueueEntry<T>> {
+        self.priority_queue
+    }
+    pub fn from_heap(heap: BinaryHeap<TimeQueueEntry<T>>) -> TimeQueue<T> {
+        TimeQueue {
+            priority_queue: heap,
+        }
+    }
+}
+#[derive(Copy, Clone, Default, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+#[repr(transparent)]
+pub struct TimeQueueSlotKey(slotmap::KeyData);
+
+impl From<slotmap::KeyData> for TimeQueueSlotKey {
+    fn from(k: slotmap::KeyData) -> Self {
+        TimeQueueSlotKey(k)
+    }
+}
+
+impl From<TimeQueueSlotKey> for slotmap::KeyData {
+    fn from(k: TimeQueueSlotKey) -> Self {
+        k.0
+    }
+}
+
+impl slotmap::Key for TimeQueueSlotKey {}
+
+slotmap::__serialize_key!(TimeQueueSlotKey);
+
+pub struct SlottedTimeQueue<T> {
+    queue: TimeQueue<TimeQueueSlotKey>,
+    slots: slotmap::DenseSlotMap<TimeQueueSlotKey, T>,
+}
+
+impl<T> SlottedTimeQueue<T> {
+    pub fn with_capacity(capacity: usize) -> SlottedTimeQueue<T> {
+        SlottedTimeQueue {
+            queue: TimeQueue::with_capacity(capacity),
+            slots: slotmap::DenseSlotMap::with_capacity_and_key(capacity),
+        }
+    }
+    pub fn push(&mut self, by_when: Timestamp, item: T) -> TimeQueueSlotKey {
+        let slot = self.slots.insert(item);
+        self.queue.push(by_when, slot);
+        slot
+    }
+    pub fn get_slot_item(&self, slot: TimeQueueSlotKey) -> Option<&T> {
+        self.slots.get(slot)
+    }
+    pub fn map_ready(&mut self, mut func: impl FnMut(T)) {
+        let queue = &mut self.queue;
+        let slots = &mut self.slots;
+        queue.map_ready_item(|slot| {
+            if let Some(item) = slots.remove(slot) {
+                func(item)
+            }
+        });
+    }
+}
--- src/time.rs
+++ src/time.rs
@@ -1,73 +1,22 @@
-use core::convert::TryFrom;
-use core::num::FpCategory::Nan;
+use alloc::collections::BinaryHeap;
+use core::cmp::Ordering;
 use core::time::Duration;
 
-#[derive(Copy, Clone, PartialOrd, PartialEq, Debug)]
-pub struct Milliseconds(u32);
-#[derive(Copy, Clone, PartialOrd, PartialEq, Debug)]
-pub struct Microseconds(u32);
-#[derive(Copy, Clone, PartialOrd, PartialEq, Debug)]
-pub struct Nanoseconds(u32);
+#[derive(Ord, PartialOrd, Eq, PartialEq, Clone, Copy, Debug, Hash)]
+pub struct Timestamp {}
 
-impl TryFrom<Milliseconds> for Microseconds {
-    type Error = ();
-
-    fn try_from(v: Milliseconds) -> Result<Microseconds, Self::Error> {
-        Ok(Microseconds((v.0).checked_mul(1000).ok_or(())?))
+impl Timestamp {
+    pub fn now() -> Timestamp {
+        unimplemented!()
     }
-}
-
-impl TryFrom<Milliseconds> for Nanoseconds {
-    type Error = ();
-
-    fn try_from(v: Milliseconds) -> Result<Nanoseconds, Self::Error> {
-        Ok(Nanoseconds((v.0).checked_mul(1000 * 1000).ok_or(())?))
+    pub fn duration_since(&self, other: &Timestamp) -> Option<Duration> {
+        if other > self {
+            None
+        } else {
+            Some(unimplemented!())
+        }
     }
-}
-
-impl From<Microseconds> for Milliseconds {
-    fn from(v: Microseconds) -> Milliseconds {
-        Milliseconds((v.0) / 1000)
-    }
-}
-
-impl TryFrom<Microseconds> for Nanoseconds {
-    type Error = ();
-
-    fn try_from(v: Microseconds) -> Result<Nanoseconds, Self::Error> {
-        Ok(Nanoseconds((v.0).checked_mul(1000).ok_or(())?))
-    }
-}
-
-impl From<Nanoseconds> for Microseconds {
-    fn from(v: Nanoseconds) -> Microseconds {
-        Microseconds((v.0) / 1000)
-    }
-}
-
-impl From<Nanoseconds> for Milliseconds {
-    fn from(v: Nanoseconds) -> Milliseconds {
-        Milliseconds((v.0) / (1000 * 1000))
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use crate::time::*;
-    use core::convert::{TryFrom, TryInto};
-
-    #[test]
-    fn test_into() {
-        let milli = Milliseconds(1000);
-        let nano = Nanoseconds(1000 * 1000 * 1000);
-        let big_milli = Milliseconds(1000 * 1000);
-        assert_eq!(
-            Microseconds::try_from(milli).unwrap(),
-            Microseconds(1000 * 1000)
-        );
-        assert_eq!(Nanoseconds::try_from(milli).unwrap(), nano);
-
-        assert!(Microseconds::try_from(big_milli).is_ok());
-        assert!(Nanoseconds::try_from(big_milli).is_err());
+    pub fn duration_until(&self, other: &Timestamp) -> Option<Duration> {
+        other.duration_since(self)
     }
 }
