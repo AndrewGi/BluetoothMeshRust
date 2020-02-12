e2d0bba163509d89f1d9d57e18ba071386915b05
AndrewGi <andrew@gilbrough.com>
2020-02-11 21:39:53 -0800

    redid HCI socket filter to be more useful and so the filter can be set each time a command is set.

--- cli/src/commands/ble/hci/dump.rs
+++ cli/src/commands/ble/hci/dump.rs
@@ -32,10 +32,11 @@ pub fn dump_bluez(_: u16, _: &slog::Logger) -> Result<(), CLIError> {
 }
 #[cfg(unix)]
 pub fn dump_bluez(adapter_id: u16, parent_logger: &slog::Logger) -> Result<(), CLIError> {
+    use btle::hci::le::SetScanEnable;
     use btle::hci::socket;
     use core::convert::TryFrom;
-    use btle::hci::le::SetScanEnable;
     let map_hci_socket_err = |err: socket::HCISocketError| match err {
+        socket::HCISocketError::BadData => CLIError::Other("hci socket bad data".to_owned()),
         socket::HCISocketError::PermissionDenied => CLIError::PermissionDenied,
         socket::HCISocketError::IO(io) => CLIError::IOError("hci socket IO error".to_owned(), io),
         socket::HCISocketError::DeviceNotFound => {
@@ -59,17 +60,24 @@ pub fn dump_bluez(adapter_id: u16, parent_logger: &slog::Logger) -> Result<(), C
         .get_adapter_socket(socket::AdapterID(0))
         .map_err(map_hci_socket_err)?;
     info!(logger, "opened socket");
-    let mut runtime = tokio::runtime::Builder::new().enable_all().build().expect("can't make async runtime");
+    let mut runtime = tokio::runtime::Builder::new()
+        .enable_all()
+        .build()
+        .expect("can't make async runtime");
     info!(logger, "starting async loop");
     runtime.block_on(async move {
         use futures::StreamExt;
         let mut async_socket = socket::AsyncHCISocket::try_from(socket)
             .map_err(|e| map_hci_socket_err(socket::HCISocketError::IO(e)))?;
         let mut stream = btle::hci::stream::ByteStream::new(&mut async_socket);
-        stream.send_command(SetScanEnable{
-            is_enabled: true,
-            filter_duplicates: false
-        }).expect("correctly formatted command").await.expect("io_error");
+        stream
+            .send_command(SetScanEnable {
+                is_enabled: false,
+                filter_duplicates: false,
+            })
+            .expect("correctly formatted command")
+            .await
+            .expect("io_error");
         info!(logger, "sent_enable");
         loop {
             let next = stream.read_event().await;
